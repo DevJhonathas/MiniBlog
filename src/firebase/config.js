4b2a49976e194d16ba6663a18a7d312d6d6eebc2
Jhonathas <jhonathassouza8@gmail.com>
2023-02-14 21:54:27 -0300

    Configurando firebase, importando fontes, importando react-router-dom, configurando css base, ajustando alguns links

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJ_NKcRew6q3HFRI1adt0eDqoz4vX6KCw",
+  authDomain: "mini-blog-68ef7.firebaseapp.com",
+  projectId: "mini-blog-68ef7",
+  storageBucket: "mini-blog-68ef7.appspot.com",
+  messagingSenderId: "58080511666",
+  appId: "1:58080511666:web:782cca422e45ba4daaebab"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const  db = getFirestore(app);
+
+export { db };
