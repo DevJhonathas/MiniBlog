5cc26d33f1a19d8ec5b5e96d4d985e8ceb301575
Jhonathas <jhonathassouza8@gmail.com>
2023-02-20 16:39:54 -0300

    Exibindo resultados de busca, criando pagina de post individual, exibindo dado de post individual, CSS da pagina de post individual

--- src/App.js
+++ src/App.js
@@ -21,11 +21,13 @@ import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
+import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import Search from './pages/Search/Search';
+
 
 
 function App() {
@@ -55,6 +57,7 @@ function App() {
             <Route path='/' element={<Home/>} />
             <Route path='/about' element={<About/>} />
             <Route path='/search' element={<Search/>} />
+            <Route path='/posts/:id' element={<Post/>} />
             <Route path='/login' element={!user ? <Login/> : <Navigate to="/" />} />
             <Route path='/register' element={!user ? <Register/> : <Navigate to="/" />} />
             <Route path='/post/create' element={user ? <CreatePost/> : <Navigate to="/login" />} />
